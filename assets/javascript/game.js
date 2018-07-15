1fef93592eb72d6b3fe0bdcfb004728f735b930f
Chace Powell <cpcpowell8@gmail.com>
2018-07-15 09:03:30 -0400

    begin adding logic for character list generation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,66 @@
-const characterBox = $('#characterBox');
-const playerBox = $('#playerBox');
-const enemyBox = $('#enemyBox');
-const actionBox = $('#actionBox');
-const logBox = $('#logBox');
-const playerPortrait = $('#playerPortrait');
-const enemyPortrait = $('#enemyPortrait');
+var characterBox = $('#characterBox');
+var playerBox = $('#playerBox');
+var enemyBox = $('#enemyBox');
+var actionBox = $('#actionBox');
+var logBox = $('#logBox');
+var playerPortrait = $('#playerPortrait');
+var enemyPortrait = $('#enemyPortrait');
+var characterHeader = $('#characterHeader');
+var characterSelect = $('#characterSelect');
 
-let gameObj = {
-    characters: {
-        players: {
-            playerAtk: 6,
-            playerHp: 120,
-            rey: {
-                pic: 'assets/images/rey.jpeg'
-            },
-            r2d2: {
-                pic: 'assets/images/r2d2.jpg'
-            },
-            luke: {
-                pic: 'assets/images/luke.jpg'
-            },
-            chewy: {
-                pic: 'assets/images/chewy.png'
-            }
-        },
-        enemies: {
-            roger: {
-                hp: 30,
-                counterAtk: 6,
-                pic: 'assets/images/roger.png'
-            },
-            stormtrooper: {
-                hp: 60,
-                counterAtk: 10,
-                pic: 'assets/images/stormtrooper.jpeg'
-            },
-            darthMaul: {
-                hp: 90,
-                counterAtk: 20,
-                pic: 'assets/images/darthMaul.jpg'
-            },
-            darthVader: {
-                hp: 120,
-                counterAtk: 30,
-                pic: 'assets/images/darthVader.jpg'
-            },
-            kylo: {
-                hp: 200,
-                counterAtk: 50,
-                pic: 'assets/images/kylo.jpg'
-            }
-        }
+var gameObj = {
+  playerAtk: 6,
+  playerHp: 120,
+  characters: {
+    players: {
+        rey: "assets/images/rey.jpeg",
+        r2d2: "assets/images/r2d2.jpg",
+        luke: "assets/images/luke.jpg",
+        chewy: "assets/images/chewy.png"
+    },
+    enemies: {
+      roger: {
+        hp: 30,
+        counterAtk: 6,
+        pic: "assets/images/roger.png"
+      },
+      stormtrooper: {
+        hp: 60,
+        counterAtk: 10,
+        pic: "assets/images/stormtrooper.jpeg"
+      },
+      darthMaul: {
+        hp: 90,
+        counterAtk: 20,
+        pic: "assets/images/darthMaul.jpg"
+      },
+      darthVader: {
+        hp: 120,
+        counterAtk: 30,
+        pic: "assets/images/darthVader.jpg"
+      },
+      kylo: {
+        hp: 200,
+        counterAtk: 50,
+        pic: "assets/images/kylo.jpg"
+      }
     }
-}
+  },
+  playerSelect: function() {
+    var port = this.characters.players;
+    $.each(port, function(key, val) {
+      var newImg = $('<img>');
+      newImg.attr('src', val);
+      $('#characterSelect').append(newImg);
+    });
+  },
+  enemySelector: function() {
+    var port = this.characters.enemies;
+    $.each(port, function(key1, val1) {
+        
+    });
+    console.log(port);
+  }
+};
+gameObj.playerSelect();
+gameObj.enemySelector();
