61c649d74624a679259e5dc6150e2305fbe2ab78
Chace Powell <cpcpowell8@gmail.com>
2018-07-18 14:35:27 -0400

    continue character balance. to chaange win and lose alerts to document writes. finish log text.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,37 +11,39 @@ var characterSelect = $('#characterSelect');
 var gameObj = {
   playerChosen: false,
   enemyChosen: false,
+  playerVal: '',
+  enemyVal: '',
   playerHP: 0,
-  runningHP: 0,
+  runningHP: 1,
   enemyHP: 0,
-  runningEHP: 0,
+  runningEHP: 1,
   atk: 0,
   runningAtk: 0,
   counterAtk: 0,
   wins: 0,
   characters: {
     rey: {
-      hp: 120,
-      atk: 6,
-      counterAtk: 12,
+      hp: 110,
+      atk: 8,
+      counterAtk: 30,
       pic: "assets/images/rey.jpeg"
     },
-    artoo: {
-      hp: 200,
-      atk: 20,
-      counterAtk: 20,
-      pic: "assets/images/r2d2.jpg"
+    rogerRoger: {
+      hp: 165,
+      atk: 5,
+      counterAtk: 8,
+      pic: "assets/images/roger.png"
     },
     luke: {
-      hp: 150,
-      atk: 8,
-      counterAtk: 15,
+      hp: 120,
+      atk: 6,
+      counterAtk: 14,
       pic: "assets/images/luke.jpg"
     },
     chewy: {
-      hp: 175,
-      atk: 7,
-      counterAtk: 10,
+      hp: 165,
+      atk: 5,
+      counterAtk: 8,
       pic: "assets/images/chewy.png"
     }
   },
@@ -73,12 +75,12 @@ var gameObj = {
       this.runningHP = this.characters.rey.hp;
       this.atk = this.characters.rey.atk;
       this.runningAtk = this.characters.rey.atk;
-    } else if (val === 'artoo') {
-      $('#playerPortrait').attr('src', this.characters.artoo.pic);
-      this.playerHP = this.characters.artoo.hp;
-      this.runningHP = this.characters.artoo.hp;
-      this.atk = this.characters.artoo.atk;
-      this.runningAtk = this.characters.artoo.atk;
+    } else if (val === 'rogerRoger') {
+      $('#playerPortrait').attr('src', this.characters.rogerRoger.pic);
+      this.playerHP = this.characters.rogerRoger.hp;
+      this.runningHP = this.characters.rogerRoger.hp;
+      this.atk = this.characters.rogerRoger.atk;
+      this.runningAtk = this.characters.rogerRoger.atk;
     } else if (val === 'luke') {
       $('#playerPortrait').attr('src', this.characters.luke.pic);
       this.playerHP = this.characters.luke.hp;
@@ -92,6 +94,7 @@ var gameObj = {
       this.atk = this.characters.chewy.atk;
       this.runningAtk = this.characters.chewy.atk;
     }
+    this.playerVal = val;
   },
   enemySelect: function(val) {
     if (val === 'rey') {
@@ -99,11 +102,11 @@ var gameObj = {
       this.enemyHP = this.characters.rey.hp;
       this.runningEHP = this.characters.rey.hp;
       this.counterAtk = this.characters.rey.counterAtk;
-    } else if (val === 'artoo') {
-      $('#enemyPortrait').attr('src', this.characters.artoo.pic);
-      this.enemyHP = this.characters.artoo.hp;
-      this.runningEHP = this.characters.artoo.hp;
-      this.counterAtk = this.characters.artoo.counterAtk;
+    } else if (val === 'rogerRoger') {
+      $('#enemyPortrait').attr('src', this.characters.rogerRoger.pic);
+      this.enemyHP = this.characters.rogerRoger.hp;
+      this.runningEHP = this.characters.rogerRoger.hp;
+      this.counterAtk = this.characters.rogerRoger.counterAtk;
     } else if (val === 'luke') {
       $('#enemyPortrait').attr('src', this.characters.luke.pic);
       this.enemyHP = this.characters.luke.hp;
@@ -115,11 +118,9 @@ var gameObj = {
       this.runningEHP = this.characters.chewy.hp;
       this.counterAtk = this.characters.chewy.counterAtk;
     }
+    this.enemyVal = val;
   },
-  atkSequence: function() {
-    var atk = this.atk;
-    var runAtk = this.runningAtk;
-    var cAtk = this.counterAtk;
+  updateHPBar: function() {
     var HP = this.playerHP;
     var eHP = this.enemyHP;
     var runHP = this.runningHP;
@@ -141,6 +142,7 @@ $('.charDiv').on('click', function() {
   if (gameObj.enemyChosen === true) {
     return;
   } else if (gameObj.playerChosen === false) {
+    $('#characterHeader').text('Choose an enemy!');
     gameObj.playerSelect(val);
     $(this).hide();
     gameObj.playerChosen = true;
@@ -148,20 +150,47 @@ $('.charDiv').on('click', function() {
     gameObj.enemySelect(val);
     $(this).hide();
     gameObj.enemyChosen = true;
-    gameObj.atkSequence();
+    gameObj.updateHPBar();
   }
 });
 
 $('#atkBtn').on('click', function() {
   if (gameObj.enemyChosen === true) {
-    gameObj.runningAtk = gameObj.runningAtk + gameObj.atk;
+    $('#playerLog').text('You attack the enemy for ' + gameObj.runningAtk + ' damage. ');
+    $('#enemyLog').text('The enemy counter attacks you for ' + gameObj.counterAtk + ' damage.');
     gameObj.runningEHP = gameObj.runningEHP - gameObj.runningAtk;
     gameObj.runningHP = gameObj.runningHP - gameObj.counterAtk;
-    gameObj.atkSequence();
+    gameObj.runningAtk = gameObj.runningAtk + gameObj.atk;
+    gameObj.updateHPBar();
+    $('#playerLog').append('(Next attack: ' + gameObj.runningAtk + ')');
   }
   if (gameObj.runningEHP <= 0) {
-    console.log('FOOBAR');
-
+    $('#enemyLog').empty();
+    $('#playerLog').text("You've defeated " + gameObj.enemyVal + "!");
+    $('#enemyPortrait').attr('src', 'assets/images/empire.png');
+    $('#enemyBar').attr('style', 'width: 100%');
+    $('#enemyBar').text('');
+    gameObj.enemyChosen = false;
+    gameObj.wins++;
+    gameObj.runningEHP = 1;
+    console.log(gameObj.wins);
+    if (gameObj.wins === 3) {
+      var win = confirm('YOU WIN\nPress OK to restart');
+      if (win === true) {
+        location.reload();
+      } else {
+        var welp = setInterval(() => {
+          $('#characterHeader').text('Please refresh the page.\nThis is awkward.');
+          clearInterval(welp);
+        }, 5000);
+      }
+    }
   }
+  if (gameObj.runningHP <= 0) {
+    var lose = confirm('YOU LOSE\nPress OK to try again');
+    if (lose === true) {
+      location.reload();
+    }
+  } 
 });
 
